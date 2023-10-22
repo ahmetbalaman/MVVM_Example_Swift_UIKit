d9bd97c6862ead949f7bc9b116cb6c1bf34fd11b
Ahmet Balaman <56163828+ahmetbalaman@users.noreply.github.com>
2023-10-22 16:02:08 +0300

    Initial Commit

---
+++ MVVM_Example_Swift_UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_Example_Swift_UIKit
+//
+//  Created by Ahmet Balaman on 22.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
